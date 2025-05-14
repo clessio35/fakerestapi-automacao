c82ad5afecc47c2889ede19871cc0cccf8815fce
Clessio <clessio.silva1989@gmail.com>
2025-05-14 17:33:41 -0300

    hooks method

--- src/main/java/fakerest/utils/Hooks.java
+++ src/main/java/fakerest/utils/Hooks.java
@@ -1,5 +1,19 @@
 package fakerest.utils;
 
+import io.cucumber.java.Before;
+
+import io.cucumber.java.Scenario;
+
 public class Hooks {
 
+	private static String scenarioName;
+
+    @Before
+    public void before(Scenario scenario) {
+        scenarioName = scenario.getName();
+    }
+
+    public static String getScenarioName() {
+        return scenarioName;
+    }
 }
--- src/test/java/fakerest/runner/RunnerTest.java
+++ src/test/java/fakerest/runner/RunnerTest.java
@@ -1,5 +1,19 @@
 package fakerest.runner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"fakerest.steps", "fakerest.utils"},
+    tags = "@all", 
+    publish = true,
+    plugin = {"pretty", "html:target/cucumber-reports"}
+)
 public class RunnerTest {
 
 }
+
