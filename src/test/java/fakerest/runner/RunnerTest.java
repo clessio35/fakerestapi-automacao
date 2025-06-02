b9d45ac9bf4d3a2245b8967750f5251bb03088bd
Clessio <clessio.silva1989@gmail.com>
2025-06-02 08:58:41 -0300

    all methods

--- src/test/java/fakerest/runner/RunnerTest.java
+++ src/test/java/fakerest/runner/RunnerTest.java
@@ -9,7 +9,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = {"fakerest.steps", "fakerest.utils"},
-    tags = "@nonexistent-book", 
+    tags = "@all", 
     publish = true,
     plugin = {"pretty", "html:target/cucumber-reports"}
 )
